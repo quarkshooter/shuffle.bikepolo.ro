928475e37e7f0b88accc9e3463a9ee53b2df3293
rcornea <radu.l.cornea@gmail.com>
2016-11-01 02:50:52 +0200

    adding new files to the project>> playerList.json and nav.js

---
+++ js/nav.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $("#my-menu").mmenu({
+    // options
+  }, {
+    // configuration
+    offCanvas: {
+      pageSelector: "#pagediv"
+    }
+  });
+  $("#lostmenu").mmenu();
+});
